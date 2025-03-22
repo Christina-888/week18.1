d21f96eb03660f0dad87ec198d99d4c53ea21177
Christina-888 <christinabykadorova@gmail.com>
2025-03-22 16:07:33 +0300

    add js code

--- assets/script/index.js
+++ assets/script/index.js
@@ -9,12 +9,26 @@ let allTasks = JSON.parse(localStorage.getItem('tasks')) || [];
 //Показываем список задач:
 const showTasks = () => {
   taskList.innerHTML = '';
-  allTasks.forEach((task) => { 
-    const liElement = document.createElement("li");
+  
+
+//Если нет задач:
+const informMessage = document.querySelector('.inform-message');
+
+if (allTasks.length === 0) {
+  informMessage.style.display = 'block';
+  clearButton.disabled = true;
+} else {
+  informMessage.style.display = 'none';
+  clearButton.disabled = false;
+};
+
+allTasks.forEach((task) => { 
+  const liElement = document.createElement("li");
 
 //Чекбоксы:
 const check = document.createElement('input');
 check.type = 'checkbox';
+check.style.marginLeft = '1%';
 check.addEventListener('change', () => {
   localStorage.setItem('tasks', JSON.stringify(allTasks));
 });
